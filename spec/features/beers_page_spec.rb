83fc05cb08ed1ae03df87a348942c54d0a22b3d9
lahdeero <eero.lahdenpera@gmail.com>
2017-02-26 22:57:03 +0200

    kuutos viikkoa

--- spec/features/beers_page_spec.rb
+++ spec/features/beers_page_spec.rb
@@ -1,26 +1,26 @@
 require 'rails_helper'
-describe "Beers page" do
-    before :each do
-      @breweries = ["Koff", "Karjala", "Schlenkerla"]
-      year = 1896
-      @breweries.each do |brewery_name|
-        FactoryGirl.create(:brewery, name: brewery_name, year: year += 1)
-      end
-      FactoryGirl.create :user2
-      sign_in(username:"Seppo", password:"Foobar2")
-      visit new_beer_path
-    end
-    it "Try to create beer without name" do
 
-      fill_in('beer_name', with:'')
+include Helpers
+
+describe "Beers" do
+  before :each do
+    FactoryGirl.create :user2
+    FactoryGirl.create :brewery, name: "Schlenkerla"
+    sign_in(username:"Seppo", password:"Foobar2")
+    visit new_beer_path
+  end
+
+  it "can be added if valid name given" do
+    fill_in('beer_name', with:'Urbock')
+    expect{
       click_button('Create Beer')
-      expect(page).to have_content "Name can't be blank"
-    end
-    it "Can create beer with name" do
-      fill_in('beer_name', with:'Kalja')
+    }.to change{Beer.count}.by(1)
+  end
 
-      expect{
-        click_button('Create Beer')
-      }.to change{Beer.count}.by(1)
-    end
+  it "can not be added if without name" do
+    expect{
+      click_button('Create Beer')
+    }.to change{Beer.count}.by(0)
+    expect(page).to have_content "Name can't be blank"
+  end
 end
